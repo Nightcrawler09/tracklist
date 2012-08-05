4101bb8416c11ef057a66662c451c37267524b8c
Fran <fran@fran-K53SJ.(none)>
2012-08-05 20:22:59 +0100

    my_first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracklist::Application.config.secret_token = 'dd928e2e5d8cd7b1790cc46c0a69de1845070c3ec67e03cf6340c2b27ce1c6c8b4a19680329a7fe2074ae47770e9df9a31a16534d5cd59984a8e7cd93a4b2586'
