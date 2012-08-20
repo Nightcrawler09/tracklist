e668f598d15813fd815920dc6d535d602b92f808
Fran <fran@fran-K53SJ.(none)>
2012-08-20 23:47:38 +0100

    added token_imputs to other models to finish the branch

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,13 @@ class Author < ActiveRecord::Base
   has_many :tracks
   has_many :taglinks, as: :resource
   has_many :tags, through: :taglinks
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :token_inputs
   validates :name, presence: true
+  attr_reader :token_inputs
+  def token_inputs=(names)
+    names.split(",").each do |name|
+      name.slice!("New: ")
+      self.tags << Tag.find_or_create_by_name(name.delete('"'))
+    end
+  end 
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,15 @@ class Track < ActiveRecord::Base
   belongs_to :author
   has_many :taglinks, as: :resource
   has_many :tags, through: :taglinks
-  attr_accessible :album_id, :author_id, :track_number, :tag_id, :title
-  validates :title, :album, :author, presence: true
+  attr_accessible :album_id, :author_id, :track_number, :title, :token_inputs
+  validates :title, :album, :author, presence: true 
+
+  attr_reader :token_inputs
+  def token_inputs=(names)
+    names.split(",").each do |name|
+      name.slice!("New: ")
+      self.tags << Tag.find_or_create_by_name(name.delete('"'))
+    end
+  end
+
 end
